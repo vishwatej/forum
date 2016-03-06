f459002a8a6c9570f18e425c0ebecaa15bb1a8fd
vishwa <vishwatej112@gmail.com>
2016-03-06 16:47:46 +0530

    Initial Commit

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../home/vt/Downloads/yii/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
